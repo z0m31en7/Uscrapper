6c307f3094579133f23bc39a47ef330bbb118018
Pranjal Goel <Pranjal.goel6@gmail.com>
2023-05-31 21:59:46 +0530

    updated phone number regex to support multiple variations

--- Uscrapper.py
+++ Uscrapper.py
@@ -31,10 +31,12 @@ def extract_details(url, generate_report, non_strict):
 
     email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
     extracted_emails = set(re.findall(email_regex, webpage_text))
-
-    phone_regex = r'\b(?:\+\d{1,3}[- ]?)?\(?\d{3}\)?[- ]?\d{3}\)?[- ]?\d{4}\b'
+    phone_regex3 = r'\(\d{3}\)\s\d{3}\s\d{5}'
+    phone_regex = r'\b\+?\d{10,12}\b'
+    phone_regex2 = r'\b(?:\+\d{1,3}[- ]?)?\(?\d{3}\)?[- ]?\d{3}\)?[- ]?\d{4}\b'
     extracted_phone_numbers = set(re.findall(phone_regex, webpage_text))
-
+    extracted_phone_numbers2 = set(re.findall(phone_regex2, webpage_text))
+    extracted_phone_numbers3 = set(re.findall(phone_regex3, webpage_text))
     username_regex = r'@[A-Za-z0-9_]+'
     extracted_usernames = set(re.findall(username_regex, webpage_text))
 
@@ -106,6 +108,18 @@ def extract_details(url, generate_report, non_strict):
     if extracted_phone_numbers:
         print(colored("\n[+] Phone Numbers:", "cyan"))
         for phone in extracted_phone_numbers:
+            print(phone)
+
+    if extracted_phone_numbers2 != extracted_phone_numbers:
+        if not extracted_phone_numbers:
+            print(colored("\n[+] Phone Numbers:", "cyan"))
+        for phone in extracted_phone_numbers2:
+            print(phone)
+
+    if extracted_phone_numbers3 != extracted_phone_numbers or extracted_phone_numbers2:
+        if not extracted_phone_numbers and extracted_phone_numbers2:
+            print(colored("\n[+] Phone Numbers:", "cyan"))
+        for phone in extracted_phone_numbers3:
             print(phone)
 
     if extracted_usernames and non_strict:
